dccbe836447db6af1fb3169e42dbc394b91217c4
Mary Skywalker <snowdennaruto90@gmail.com>
2023-01-03 16:15:29 -0400

    first commit

---
+++ src/styles/theme.ts
@@ -0,0 +1,29 @@
+import { extendTheme } from 'native-base';
+
+export const THEME = extendTheme({
+  colors: {
+    primary: {
+      700: "#584581",
+    },
+
+  gray: {
+      700: "#121214",
+      600: "#202024",
+      400: "#7C7C8A",
+    },
+    white: "#FFFFFF",
+  },
+  fonts: {
+    heading: 'Roboto_700Bold',
+    body: 'Roboto_600Regular',
+  },
+  fontSize: {
+    xs: 12,
+    sm: 14,
+    md: 16,
+    lg: 20
+  },
+  sizes: {
+    14: 56,
+  },
+});
