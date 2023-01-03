f3f1eeb75f1f7b15ae6c17885f2192b6da6e0f88
Mary Skywalker <snowdennaruto90@gmail.com>
2023-01-03 17:02:09 -0400

    signin screen structure

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,9 +1,11 @@
-import { VStack } from 'native-base';
+import { VStack, Heading } from 'native-base';
 
 export function SignIn(){
     return(
         <VStack flex={1} alignItems="center" bg="gray.700" px={8} pt={24}>
-
+            <Heading color="gray.100" mt={20} mb={6}>
+                Seja bem vindo!
+            </Heading>
         </VStack>
     );
 }
