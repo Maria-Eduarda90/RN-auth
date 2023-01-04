27c2538a8244df4af2cc517504136c5e2b5a7e04
Mary Skywalker <snowdennaruto90@gmail.com>
2023-01-03 20:10:47 -0400

    added component Input

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,11 +1,25 @@
-import { VStack, Heading } from 'native-base';
+import { VStack, Heading, Text, Icon } from 'native-base';
+import { SimpleLineIcons, MaterialCommunityIcons } from '@expo/vector-icons';
+import { Input } from '../components/Input';
 
 export function SignIn(){
     return(
         <VStack flex={1} alignItems="center" bg="gray.700" px={8} pt={24}>
-            <Heading color="gray.100" mt={20} mb={6}>
+            <Heading color="gray.100" mt={20} mb={4}>
                 Seja bem vindo!
             </Heading>
+            <Text color="gray.400" fontSize="xl" mb={12}>Faça login e comece a usar</Text>
+
+            <Input
+             placeholder="teste@example.com"
+             mb={6}
+             InputLeftElement={<Icon as={<MaterialCommunityIcons name="email-outline"/>} ml={4} size={6}/>}
+            />
+            <Input
+             placeholder="*************"
+             InputLeftElement={<Icon as={<SimpleLineIcons name="lock" />} ml={4} size={6} />}
+             secureTextEntry
+            />
         </VStack>
     );
 }
