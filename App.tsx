efd1649d5cb3635eaedad6dd3f5977c544e6587f
Mary Skywalker <snowdennaruto90@gmail.com>
2023-01-04 22:08:38 -0400

    added routes

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import { NativeBaseProvider, StatusBar } from "native-base";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import { SignIn } from "./src/screens/SignIn";
 import { THEME } from "./src/styles/theme";
 import { Loading } from "./src/components/Loading";
+import { Routes } from "./src/Routes";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -15,7 +15,7 @@ export default function App() {
         translucent
       />
       {
-        fontsLoaded ? <SignIn /> : <Loading/> 
+        fontsLoaded ? <Routes /> : <Loading/> 
       }
     </NativeBaseProvider>
   );
