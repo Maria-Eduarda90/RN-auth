745de5a4ef5aab6d56e6b8edd93794d1b77122bf
Mary Skywalker <snowdennaruto90@gmail.com>
2023-01-04 22:12:16 -0400

    feat: openScreen function created to go to signup screen

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -2,8 +2,20 @@ import { VStack, Heading, Text, Icon } from 'native-base';
 import { SimpleLineIcons, MaterialCommunityIcons } from '@expo/vector-icons';
 import { Input } from '../components/Input';
 import { Button } from '../components/Button';
+import { useState } from 'react';
 
-export function SignIn(){
+export function SignIn({ navigation }){
+    const [name, setName] = useState<string>();
+    const [password, setPassword] = useState<string>();
+
+    function openScreen(){
+        navigation.navigate('signup');
+    }
+
+    function handleSignIn(){
+        console.log(name, password);
+    }
+
     return(
         <VStack flex={1} alignItems="center" bg="gray.700" px={8} pt={24}>
             <Heading color="gray.100" mt={20} mb={4}>
@@ -15,15 +27,19 @@ export function SignIn(){
              placeholder="teste@example.com"
              mb={4}
              InputLeftElement={<Icon as={<MaterialCommunityIcons name="email-outline"/>} ml={4} size={6}/>}
+             onChangeText={setName}
             />
             <Input
              mb={10}
              placeholder="*************"
              InputLeftElement={<Icon as={<SimpleLineIcons name="lock" />} ml={4} size={6} />}
              secureTextEntry
+             onChangeText={setPassword}
             />
 
-            <Button title="Entrar" w="full" />
+            <Button title="Entrar" w="full" onPress={handleSignIn}/>
+
+            <Heading color="gray.100" fontSize="sm" mt={8}>Ainda não possui uma conta? <Text onPress={openScreen}> cadastre-se</Text></Heading>
         </VStack>
     );
 }
