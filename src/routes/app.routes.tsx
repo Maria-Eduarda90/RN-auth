ba3c6e40055134da37766924b2e5afd1e4863b83
Mary Skywalker <snowdennaruto90@gmail.com>
2023-01-06 18:59:13 -0400

    home screen added in application

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SignIn } from '../screens/SignIn';
 import { SignUp } from '../screens/SignUp';
+import { Home } from '../screens/Home';
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -9,6 +10,7 @@ export function AppRoutes(){
         <Navigator screenOptions={{ headerShown: false }}>
             <Screen name="signin" component={SignIn} />
             <Screen name="signup" component={SignUp} />
+            <Screen name="home" component={Home} />
         </Navigator>
     );
 }
